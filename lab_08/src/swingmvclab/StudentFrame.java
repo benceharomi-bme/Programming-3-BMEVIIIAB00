8b0827b46e56fd56eb812c638cdf843a8dfdf4db
Bence Háromi <56651250+benceharomi@users.noreply.github.com>
2019-11-04 16:07:35 +0100

    Add files via upload

---
+++ lab_08/src/swingmvclab/StudentFrame.java
@@ -0,0 +1,112 @@
+package swingmvclab;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.List;
+import javax.swing.*;
+
+/*
+ * A megjelenítendõ ablakunk osztálya.
+ */
+public class StudentFrame extends JFrame {
+    JTextField nameField;
+    JTextField neptunField;
+    /*
+     * Ebben az objektumban vannak a hallgatói adatok.
+     * A program indulás után betölti az adatokat fájlból, bezáráskor pedig kimenti oda.
+     * 
+     * NE MÓDOSÍTSD!
+     */
+    private StudentData data;
+    
+    /*
+     * Itt hozzuk létre és adjuk hozzá az ablakunkhoz a különbözõ komponenseket
+     * (táblázat, beviteli mezõ, gomb).
+     */
+    private void initComponents() {
+        this.setLayout(new BorderLayout());
+        JTable table = new JTable(data);
+        table.setFillsViewportHeight(true);
+        this.add(new JScrollPane(table),BorderLayout.CENTER);
+        JPanel panel = new JPanel(new FlowLayout());
+        panel.add(new JLabel("Név:"));
+        nameField = new JTextField(20);
+        panel.add(nameField);
+        panel.add(new JLabel("Neptun:"));
+        neptunField = new JTextField(6);
+        panel.add(neptunField);
+        JButton button = new JButton("Felvesz");
+        button.addActionListener(new MyActionListener());
+        panel.add(button);
+        this.add(panel,BorderLayout.SOUTH);
+
+    }
+
+    public class MyActionListener implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String name = nameField.getText();
+            String neptun = neptunField.getText();
+            data.addStudent(name,neptun);
+
+        }
+    }
+
+    /*
+     * Az ablak konstruktora.
+     * 
+     * NE MÓDOSÍTSD!
+     */
+    @SuppressWarnings("unchecked")
+    public StudentFrame() {
+        super("Hallgatói nyilvántartás");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        
+        // Induláskor betöltjük az adatokat
+        try {
+            data = new StudentData();
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream("students.dat"));
+            data.students = (List<Student>)ois.readObject();
+            ois.close();
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
+        
+        // Bezáráskor mentjük az adatokat
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                try {
+                    ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("students.dat"));
+                    oos.writeObject(data.students);
+                    oos.close();
+                } catch(Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        // Felépítjük az ablakot
+        setMinimumSize(new Dimension(500, 200));
+        initComponents();
+    }
+
+    /*
+     * A program belépési pontja.
+     * 
+     * NE MÓDOSÍTSD!
+     */
+    public static void main(String[] args) {
+        // Megjelenítjük az ablakot
+        StudentFrame sf = new StudentFrame();
+        sf.setVisible(true);
+    }
+}
